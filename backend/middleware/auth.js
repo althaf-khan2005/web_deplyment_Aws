76ce969fd191055dc507cd6b1c2ecbf05e60785f
Althaf <althaf@gmail.com>
2026-08-15 21:27:11 +0530

    Backend issue fixed'

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ export function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'No token provided' });
+    return res.status(401).json({ message: 'No token psrovided' });
   }
 
   const token = authHeader.split(' ')[1];
